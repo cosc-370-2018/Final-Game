43bd4e326f7f936714e5c9187d1f8411dcc96506
originalang <angeorge200@gmail.com>
2019-03-10 23:26:50 -0700

    Created basic script to generate a procedural track

---
+++ Assets/TrackMeshGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class TrackMeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 6;
+    public int zSize = 10;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GenerateStraightSection();
+        //GenerateCurvedSection();
+        CreateMesh();
+    }
+
+    void GenerateStraightSection()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tri = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tri] = vert;
+                triangles[tri + 1] = vert + xSize + 1;
+                triangles[tri + 2] = vert + 1;
+                triangles[tri + 3] = vert + 1;
+                triangles[tri + 4] = vert + xSize + 1;
+                triangles[tri + 5] = vert + xSize + 2;
+
+                vert++;
+                tri += 6;
+            }
+            vert++;
+        }
+    }
+
+    void GenerateCurvedSection()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        float xoffset = 0f;
+        float zoffset = 0f;
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x + xoffset, 0, z + zoffset);
+                i++;
+                xoffset += 0.01f;
+                if (z > 0)
+                {
+                    zoffset -= 0.08f;
+                }
+                
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tri = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tri] = vert;
+                triangles[tri + 1] = vert + xSize + 1;
+                triangles[tri + 2] = vert + 1;
+                triangles[tri + 3] = vert + 1;
+                triangles[tri + 4] = vert + xSize + 1;
+                triangles[tri + 5] = vert + xSize + 2;
+
+                vert++;
+                tri += 6;
+            }
+            vert++;
+        }
+    }
+
+    void CreateMesh()
+    {
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int  i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }
+}
